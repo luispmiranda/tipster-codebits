4ca4f4c80c7289db211fce1ba95b654ff897d557
Miguel Palhas <mpalhas@gmail.com>
2012-11-15 22:15:23 +0000

    added skeleton rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tipster::Application.config.secret_token = '67e75f7cd4e04723d1b159838084c0911a2c1a96a7077e032b4f31aa36d67694b69936b638798d5fe6fd08a30af37b96619f1eb41ac754b8ef367acd97125959'
