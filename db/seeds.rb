81660a719d2994714880de52b1197b9d16d1f0e7
Miguel Palhas <mpalhas@gmail.com>
2012-11-16 18:00:26 +0000

    seeds fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,13 @@
 Citygate::Engine.load_seed
+
+unless Citygate::User.find_by_email('admin@tipster.com')
+  user = Citygate::User.create first_name:   'Tipster',
+                               last_name:    'Admin',
+                               email:        'admin@tipster.com',
+                               password:     'tipster',
+                               confirmed_at: Time.now
+
+  user.confirm!
+  user.role = Citygate::Role.find_by_name('Admin')
+  user.save
+end
